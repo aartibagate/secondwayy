8453520caeacdbf70ac2b64bbf2d20bfe8ef30ea
aartibagate <aartibagate232@gmail.com>
2023-10-25 11:33:59 +0530

    hello

---
+++ SpringBootcrudpractice2/src/main/java/com/example/SpringBootcrudpractice/SpringBootcrudpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootcrudpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootcrudpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootcrudpracticeApplication.class, args);
+	}
+
+}
---
+++ SpringBootcrudpractice2/src/test/java/com/example/SpringBootcrudpractice/SpringBootcrudpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootcrudpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootcrudpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
